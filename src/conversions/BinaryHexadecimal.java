48f33ad6243d91c319515180fec14dbaf8010d38
Michael Janks <mjanks@emich.edu>
2020-09-19 10:23:31 -0400

    added getParts()

--- src/conversions/BinaryHexadecimal.java
+++ src/conversions/BinaryHexadecimal.java
@@ -1,13 +1,39 @@
 package conversions;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class BinaryHexadecimal {
 	
 	public String binToHex(String bin) {
 		String hex = "";
 		
+		// Divide binary string into blocks of 4 bits each
+		
+		// Convert each 4 bit binary string to dec value (already done)
+		
+		// Convert the dec value to hex value
+		
+		// Concatenate the string to create the hex value
 		
 		
 		return hex;
 	}
+	
+	/************************************************************************************************************
+	 * 
+	 * getParts() from java2s.com  | © Demo Source and Support
+	 * http://www.java2s.com/Tutorials/Java/Algorithms_How_to/String/Split_a_string_at_every_n_th_character.htm
+	 *
+	 ***********************************************************************************************************/
+	
+	public ArrayList<String> getParts(String string, int partitionSize) {
+	    ArrayList<String> parts = new ArrayList<String>();
+	    int len = string.length();
+	    for (int i = 0; i < len; i += partitionSize) {
+	      parts.add(string.substring(i, Math.min(len, i + partitionSize)));
+	    }
+	    return parts;
+	  }
 	
 }
